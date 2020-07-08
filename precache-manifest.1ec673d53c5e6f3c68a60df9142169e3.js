0be0dfcf8ee1926cf2f2d385a6213c3acd54c91e
Dibs99 <d.n.haslett@gmail.com>
2020-07-09 09:02:24 +1200

    09 July 2020

---
+++ precache-manifest.1ec673d53c5e6f3c68a60df9142169e3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48981a28069101a58b6e46219be9a09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86a1e3d00abe4c47be6f",
+    "url": "/static/css/main.e3b76289.chunk.css"
+  },
+  {
+    "revision": "61dbc5a4ed71d4aec3bb",
+    "url": "/static/js/2.b5e937ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b5e937ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a1e3d00abe4c47be6f",
+    "url": "/static/js/main.0034cc66.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "b66af145ebcfddb4274eb8aee9b2b23e",
+    "url": "/static/media/Jamming.b66af145.jpg"
+  },
+  {
+    "revision": "2838d0615a13aa256e09d0e5a346c07a",
+    "url": "/static/media/SleekWebsite.2838d061.jpg"
+  },
+  {
+    "revision": "a6aa845cb71d54faff41fbfe151f6e31",
+    "url": "/static/media/SleekWebsitePhone.a6aa845c.png"
+  },
+  {
+    "revision": "d544adcda7aada36b9cb114ca16053ac",
+    "url": "/static/media/gig-app.d544adcd.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2e1b051bbff79ec5e73e7964bc609b33",
+    "url": "/static/media/wordpress.2e1b051b.jpg"
+  }
+]);
